db32e37d35fb20c2cd7a7474d608e0ecaf4202e3
ZQDELL <ZQ050314@126.com>
2016-06-21 09:36:01 +0800

    增加单例模式

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+   public class Singleton
+    {
+       private static Singleton  uniqueInstance = new Singleton();
+
+       private Singleton(){}
+
+       public static Singleton GetInstance()
+       {
+           return uniqueInstance;
+       }
+    }
+}
---
+++ SingletonPattern/Singleton_lock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+   public class Singleton_lock
+    {
+       private static Object lockObj = new Object();
+
+       private static Singleton_lock uniqueInstance;
+
+       private Singleton_lock()
+       {
+
+       }
+
+       public static Singleton_lock Singleton_lock()
+       {
+           lock(lockObj)
+           {
+               if(uniqueInstance == null)
+               {
+                   uniqueInstance = new Singleton_lock();
+               }
+               return uniqueInstance;
+           }
+       }
+
+    }
+}
