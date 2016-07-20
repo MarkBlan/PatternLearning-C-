511a724a5cf3977875e61156e337cbab36c8fb24
ZQDELL <ZQ050314@126.com>
2016-07-20 20:07:25 +0800

    TemplateMethodPattern added.

---
+++ TemplateMethodPattern/Coffee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern
+{
+    public class Coffee:CoffeineBeverage
+    {
+
+        private Boolean customerWantsCondiments_;
+
+        private String name_;
+
+        public String Name
+        {
+            get
+            {
+                return name_;
+            }
+            set
+            {
+                name_ = value;
+            }
+        }
+
+        public Coffee(String name, Boolean customerWantsCondiments)
+        {
+            name_ = name;
+            customerWantsCondiments_ = customerWantsCondiments;
+        }
+        public override void Brew()
+        {
+            Console.WriteLine("Brew  the {0}", name_);
+        }
+
+        public override void AddCondiments()
+        {
+            Console.WriteLine("Add the Sugar and milk in the {0}", name_);
+        }
+
+        public override bool CustomerWantsCondiments()
+        {
+            if (customerWantsCondiments_)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ TemplateMethodPattern/CoffeineBeverage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern
+{
+    public abstract class CoffeineBeverage
+    {
+        public void BoulWater()
+        {
+            Console.WriteLine("Boul The Water.");
+        }
+
+        public void PourInCup()
+        {
+            Console.WriteLine("Pour the Bervage In Cup.");
+        }
+
+        public abstract void Brew();
+
+        public abstract void AddCondiments();
+
+        public abstract Boolean CustomerWantsCondiments()
+        {
+            return true;
+        }
+
+        public  void PrepareRecipe()
+        {
+            BoulWater();
+            Brew();
+            PourInCup();
+            if(CustomerWantsCondiments())
+            {
+                AddCondiments();
+            }
+        }
+    }
+}
